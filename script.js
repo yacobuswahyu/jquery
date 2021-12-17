6245ea700809fff5bedc3db1d839a253c0d79bca
yacobuswahyu <yacobus.wibawa@students.amikom.ac.id>
2021-12-17 17:33:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('#baten').click(function(){
+        $('.ktk').fadeToggle("fast");
+    })
+});
+// $(document).ready(function(){
+//     $('#hide').click(function(){
+//         $('#kotak').hide("normal");
+//     });
+//     $('#show').click(function(){
+//         $('#kotak').show("normal");
+//     });
+//     $('#fadeout').click(function(){
+//         $('.box1').fadeOut();
+//         $('.box2').fadeOut(1000);
+//         $('.box3').fadeOut(500);
+//     });
+//     $('#fadein').click(function(){
+//         $('.box1').fadeIn();
+//         $('.box2').fadeIn(1000);
+//         $('.box3').fadeIn(500);  
+//     });
+//     $('#baten').click(function(){
+//         $('.ktk').fadeToggle("normal");
+//     })
+
+    // fadeToogle untuk hilang muncul
+// });
